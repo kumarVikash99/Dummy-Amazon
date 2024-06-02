dcd25505bea389c809d57473f71ea87b1029e301
Vikash <vkm762003@gmail.com>
2024-06-02 21:26:53 +0530

    Updates

---
+++ precache-manifest.6d1f29dc9757f98c5b82971c58b6c979.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35bbafcb4f0ec612eded5d6d27b806a9",
+    "url": "/Dummy-Amazon/index.html"
+  },
+  {
+    "revision": "d4e094a4d61aebb8d0ad",
+    "url": "/Dummy-Amazon/static/css/main.0d9e319a.chunk.css"
+  },
+  {
+    "revision": "42f62f1f4dde945e1f6a",
+    "url": "/Dummy-Amazon/static/js/2.05826798.chunk.js"
+  },
+  {
+    "revision": "2b587c35db5df3608932eb682dfa83dc",
+    "url": "/Dummy-Amazon/static/js/2.05826798.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4e094a4d61aebb8d0ad",
+    "url": "/Dummy-Amazon/static/js/main.db99134a.chunk.js"
+  },
+  {
+    "revision": "b5a4177788b97e1b1b28",
+    "url": "/Dummy-Amazon/static/js/runtime-main.e14e1fa0.js"
+  }
+]);
